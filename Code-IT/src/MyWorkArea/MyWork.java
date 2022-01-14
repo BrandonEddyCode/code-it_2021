ebb6142bd397f092b8a6f381bf7b6ddfab973e82
BrandonEddyCode <brandoneddycode@gmail.com>
2022-01-14 17:24:56 +0000

    Editing MyWork

--- Code-IT/src/MyWorkArea/MyWork.java
+++ Code-IT/src/MyWorkArea/MyWork.java
@@ -3,7 +3,7 @@ package MyWorkArea;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+//import java.awt.GridLayout;
 import java.awt.Dimension;
 import java.awt.Color;
 
@@ -24,23 +24,42 @@ MyWorkAreaItems myWorkAreaItems;
 
  public MyWork(){
      // settings of mywork
-        this.setBackground(Color.green);
-        this.setLayout(new GridLayout());
+        this.setBackground(Color.black);
+        this.setLayout(new BorderLayout(10,10));
         this.setVisible(false);
-        this.setPreferredSize(new Dimension(300,300));
+        this.setSize(500,500);
         this.setOpaque(true);
     
 
          //topsidepanel
-        
-        
-         //centrerpanel
+         JPanel panel1 = new JPanel();
+         JPanel panel2 = new JPanel();
+         JPanel panel3 = new JPanel();
+         
+
+         panel1.setBackground(Color.RED); // TOP PANEL
+         panel2.setBackground(Color.MAGENTA); // CENTER PANEL
+         panel3.setBackground(Color.GREEN);// TOP CENTER PANEL
+         
+
+
+        // panel2.setLayout(new BorderLayout(50,50));
+    
+         
+         panel1.setPreferredSize(new Dimension(100,100));
+         panel2.setPreferredSize(new Dimension(100,100));
+         panel3.setPreferredSize(new Dimension(2000, 100));
+
+         panel2.add(panel3,BorderLayout.NORTH); 
+         this.add(panel1, BorderLayout.NORTH);
+         this.add(panel2, BorderLayout.CENTER);
+
+         
+    
          //topsidecentrepanel
-         this.add(new MyWorkAreaItems("Overdue"));
-         this.add(new MyWorkAreaItems("coglionbe"));
-         this.add(new MyWorkAreaItems("Overdue"));
-         this.add(new MyWorkAreaItems("Overdue"));
-         this.add(new MyWorkAreaItems("Overdue"));
+            //this.add(new MyWorkAreaItems("Overdue"));
+
+        //centrerpanel
 
  }
 
--- Code-IT/src/MyWorkArea/MyWorkAreaItems.java
+++ Code-IT/src/MyWorkArea/MyWorkAreaItems.java
@@ -1,21 +1,32 @@
 package MyWorkArea;
-
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 
+//import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+//import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MyWorkAreaItems extends JPanel {
 
-    
+    ImageIcon image = new ImageIcon("res/Expanding_Button.jpg");
 
     MyWorkAreaItems (String nameTab) {
         JLabel a = new JLabel(nameTab);
-        a.setPreferredSize(new Dimension(200,200));
+        a.setIcon(image);
+        a.setHorizontalTextPosition(JLabel.RIGHT); // set text RIGHT of imageIcon
+        a.setPreferredSize(new Dimension(100,100));
         a.setVisible(true);
         a.setOpaque(true);
-        a.setBackground(Color.BLACK);
+        a.setBackground(Color.lightGray);
+        a.setFont(new Font("Helvetica",Font.PLAIN, 15));
+        a.setFocusable(false); 
         this.add(a);
     }
 
